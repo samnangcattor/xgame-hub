6741deff1e1865d02fcce95a9bd89302df55a4d5
dam.samnang <kcattor@gmail.com>
2023-12-31 10:26:02 -0800

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Game } from '../models/game';
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+  const platforms = game.parent_platforms.map(({ platform }) => platform);
+
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList platforms={platforms}></PlatformIconList>
       </CardBody>
     </Card>
   );
