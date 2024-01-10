53b17872a401ecfff016a7045b05a1bfdfc19f4c
dam.samnang <kcattor@gmail.com>
2024-01-10 13:50:58 -0800

    Exercise - Fetching games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,18 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
 import { Game, GameQuery } from '../models/game';
-import useData from './useData';
+import apiClient from '../services/api-client';
+import { FetchResponse } from '../models/data';
 
-const useGames = (gameQuery: GameQuery | null) =>
-  useData<Game>(
-    '/games',
-    {
-      params: {
+const useGames = (gameQuery: GameQuery | null) => {
+  const fetchGames = () =>
+    apiClient
+      .get<FetchResponse<Game>>('/games', {
+        params: {
+          genres: gameQuery?.genre?.id,
+          parent_platforms: gameQuery?.platform?.id,
+          ordering: gameQuery?.sortOrder?.value,
+          search: gameQuery?.searchText,
+        },
+      })
+      .then((res) => res.data);
+
+  return useQuery<FetchResponse<Game>, Error>({
+    queryKey: [
+      'games',
+      {
         genres: gameQuery?.genre?.id,
-        platforms: gameQuery?.platform?.id,
+        parent_platforms: gameQuery?.platform?.id,
         ordering: gameQuery?.sortOrder?.value,
         search: gameQuery?.searchText,
       },
-    },
-    [gameQuery]
-  );
+    ],
+    queryFn: fetchGames,
+  });
+};
 
 export default useGames;
